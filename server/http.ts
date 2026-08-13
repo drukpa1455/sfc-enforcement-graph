b048821ca75589c808701da5c6a8aa91afdf9bd4
drk <136856552+drukpa1455@users.noreply.github.com>
2026-08-12 20:52:40 -0700

    Stop docs redirect loop (#49)

--- server/http.ts
+++ server/http.ts
@@ -7,7 +7,10 @@ const http = new Hono({ strict: false })
 
 http.use('*', compress())
 http.route('/', api)
-http.get('/docs', (context) => context.redirect('/docs/'))
+http.use('/docs', async (context, next) => {
+  if (new URL(context.req.url).pathname === '/docs') return context.redirect('/docs/')
+  await next()
+})
 http.use('*', serveStatic({ root: './dist' }))
 http.get('*', serveStatic({ path: './dist/index.html' }))
 
